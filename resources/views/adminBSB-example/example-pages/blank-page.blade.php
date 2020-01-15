459f12c55940639b16d09f925226e7847f11057d
indra-yana <indra.ndra26@gmail.com>
2020-01-15 11:47:18 +0700

    added new files

---
+++ resources/views/adminBSB-example/example-pages/blank-page.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app_layout')
+
+@extends('layouts.section_top_nav')
+@extends('layouts.section_sidebar')
+
+@section('title', 'Welcome')
+
+@section('styles')
+  <!-- Add custom styles here -->
+@endsection
+
+@section('section_content')
+    
+@endsection
+
+@section('scripts')
+  <!-- Custom Js -->
+  <script type="text/javascript">
+    $(function(){
+      //** Do something here with JQuery **//
+    });
+  </script>
+@endsection
